e5a0d4664cd99b1754f6e0fdbc5c72fe0ebcb08f
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2024-10-07 17:50:48 +0530

    links originated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import Mainpage from './Mainpage';
-import Dashboard from '../Pages/Dashboard';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/*" element={<Mainpage />} />
     </Routes>
   );
 }
